c25231dfd70ac067e41e84275791e3900e8bb1db
Jacob Parker <jacobparker1992@gmail.com>
2020-12-30 11:07:03 +0000

    .

--- core/components/ExternalCss.tsx
+++ core/components/ExternalCss.tsx
@@ -1,19 +1,15 @@
 import useContent from "../useContent";
-import cacheAssetTransform from "../cacheAssetTransform";
 import transformCss from "../transformCss";
-
-const transform = cacheAssetTransform((content, src) => {
-  const input = src.split(",").map(content.asset).join("\n");
-  const output = transformCss(content, input);
-  const href = content.write(output, { extension: ".css" });
-  return href;
-});
 
 type Props = {
   src: string;
 };
 
 export default ({ src }: Props) => {
+  // Can't cache because this could be multiple sources
   const content = useContent();
-  return <link href={transform(content, src)} rel="stylesheet" />;
+  const input = src.split(",").map(content.asset).join("\n");
+  const output = transformCss(content, input);
+  const href = content.write(output, { extension: ".css" });
+  return <link href={href} rel="stylesheet" />;
 };
--- core/components/InlineCss.tsx
+++ core/components/InlineCss.tsx
@@ -1,18 +1,14 @@
 import useContent from "../useContent";
-import cacheAssetTransform from "../cacheAssetTransform";
 import transformCss from "../transformCss";
-
-const transform = cacheAssetTransform((content, src) => {
-  const input = src.split(",").map(content.asset).join("\n");
-  return transformCss(content, input);
-});
 
 type Props = {
   src: string;
 };
 
 export default ({ src }: Props) => {
+  // Can't cache because this could be multiple sources
   const content = useContent();
-  const output = transform(content, src);
+  const input = src.split(",").map(content.asset).join("\n");
+  const output = transformCss(content, input);
   return <style dangerouslySetInnerHTML={{ __html: output }} />;
 };
